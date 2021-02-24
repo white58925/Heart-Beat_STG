5694bb24a28b1a24080e826b36c92dcb41cf1339
EJ Huang <ej.huang@kooapps.com>
2021-02-24 21:08:54 +0800

    Add Initial Ticker

---
+++ Heart-Beat-STG/Assets/Scripts/Ticker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+enum TouchState
+{
+    Excellent,
+    Good,
+    Bad
+}
+public class Ticker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static Ticker instance;
+
+    void Awake()
+    {
+        MakeInstance();
+    }
+
+    void MakeInstance()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
