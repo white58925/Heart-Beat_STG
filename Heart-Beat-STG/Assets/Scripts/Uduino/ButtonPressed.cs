86974357986261f78e9c7adc1e19d886912c0f25
white58925 <miro287427894@gmail.com>
2021-03-24 20:56:09 +0800

    Add Uduino Scripts

---
+++ Heart-Beat-STG/Assets/Scripts/Uduino/ButtonPressed.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Uduino;
+
+public class ButtonPressd : MonoBehaviour
+{
+    public Text textZone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UduinoManager.Instance.pinMode(9, PinMode.Input_pullup);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int buttonValue = UduinoManager.Instance.digitalRead(9);
+
+        if (buttonValue == 0)
+        {
+            textZone.text = "Down";
+        }
+        else
+        {
+            textZone.text = "Up";
+        }
+    }
+}
