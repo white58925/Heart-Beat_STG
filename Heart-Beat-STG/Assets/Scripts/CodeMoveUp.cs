b951881ff700fa6b138084797fb8cfea5299acb4
white58925 <miro287427894@gmail.com>
2021-04-07 17:41:52 +0800

    Add Test Scene

---
+++ Heart-Beat-STG/Assets/Scripts/CodeMoveUp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeMoveUp : MonoBehaviour
+{
+    Vector3 Pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(0, Random.Range(0.1f, 0.05f), 0);
+        transform.position += move;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        transform.position = Pos;
+    }
+}
