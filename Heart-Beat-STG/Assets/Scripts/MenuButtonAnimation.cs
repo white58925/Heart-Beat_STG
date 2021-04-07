adb8d4b8508d78e68d852719baf379aa81fc3e81
white58925 <miro287427894@gmail.com>
2021-04-07 20:01:17 +0800

    MenuButtonScript

---
+++ Heart-Beat-STG/Assets/Scripts/MenuButtonAnimation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonAnimation : MonoBehaviour
+{
+
+    [SerializeField] Animator animator;
+    [SerializeField] UIButton uiButtonType;
+
+    void Update()
+    {
+        if (UIController.Instance.index == (int)uiButtonType)
+        {
+            animator.SetBool("selected", true);
+            if (Input.GetAxis("Submit") == 1)
+            {   
+                animator.SetBool("pressed", true);
+            }
+            else if (animator.GetBool("pressed"))
+            {
+                UIController.Instance.UIButtonClick(uiButtonType);
+                animator.SetBool("pressed", false);
+            }
+        }
+        else
+        {
+            animator.SetBool("selected", false);
+        }
+    }
+}
