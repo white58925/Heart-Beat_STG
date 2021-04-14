df7d25530f27ef0264650f54a428065738562c27
white58925 <miro287427894@gmail.com>
2021-04-14 17:06:33 +0800

    Add Eye Animation

---
+++ Heart-Beat-STG/Assets/Scripts/ReadEncoder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Uduino;
+
+public class ReadEncoder : MonoBehaviour
+{
+    void Start()
+    {
+        UduinoManager.Instance.OnDataReceived += DataReceived;
+    }
+
+    void DataReceived(string data, UduinoDevice board)
+    {
+        Debug.Log(data);
+    }
+}
